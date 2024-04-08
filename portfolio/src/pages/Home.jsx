e2fd4295482e1b6959de6080b9f5a2ae7abc3f3c
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2024-04-08 15:36:45 +0200

    islandRemovedBird
    islandRemovedBird

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -6,7 +6,6 @@ import Loader from '../components/Loader';
 
 import Island from '../models/Island';
 import Sky from '../models/Sky';
-import Bird from '../models/Bird';
 import Plane from '../models/Plane';
 
 
@@ -61,7 +60,6 @@ const Home = () => {
           <ambientLight intensity={0.5} />
           <hemisphereLight skyColor={'#b1e1ff'} groundColor={'#000000'} intensity={1} />
           
-          <Bird/>
           <Sky
             isRotating = {isRotating}
           />
