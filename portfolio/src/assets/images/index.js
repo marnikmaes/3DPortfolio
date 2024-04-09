29e9176051667f712526c3356bc5e074a105f2c5
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2024-04-09 16:25:16 +0200

    aboutMe
    aboutMe

--- portfolio/src/assets/images/index.js
+++ portfolio/src/assets/images/index.js
@@ -4,6 +4,10 @@ import shopify from './shopify.png'
 import starbucks from './starbucks.png'
 import tesla from './tesla.png'
 import logo from './logo.svg'
+import kogeka from './kogeka.png'
+import jozef from './jozef.png'
+import college from  './college.png'
+import di from './di.png'
 
 export {
     hero,
@@ -11,5 +15,9 @@ export {
     shopify,
     starbucks,
     tesla,
-    logo
+    logo,
+    kogeka,
+    jozef,
+    college,
+    di,
 }
