ae6de8954afb8c1f9db88e248715eaa4b473f0f0
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2024-04-12 14:51:24 +0200

    noContactFromJustCopyEmail
    removed the contact form because it wasn't working and the website doesn't need it for now.

--- portfolio/src/components/CTA.jsx
+++ portfolio/src/components/CTA.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
 
 const CTA = () => {
+  const copyEmailToClipboard = () => {
+    navigator.clipboard.writeText('marnikmaes46@hotmail.com')
+      .then(() => {
+        alert('You have copied my email. Please contact me!');
+      })
+      .catch((error) => {
+        console.error('Failed to copy email address: ', error);
+      });
+  };
+
   return (
     <section className='cta'>
-        <p className='cta-text'>Have any more questions for me? <br className='sm:block hidden'/> 
+      <p className='cta-text'>Have any more questions for me? <br className='sm:block hidden'/> 
         Don't hesitate to reach out!</p>
-        <Link to='/contact' className='btn'>Contact</Link>
+      <button className='btn' onClick={copyEmailToClipboard}>Copy my email</button>
     </section>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
 
     emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_,
       {
         from_name: form.name,
         to_name: 'Marnik',
