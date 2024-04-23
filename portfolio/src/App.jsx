56933785ebf995f8890b58af6c39100423f0379b
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2024-04-23 10:30:20 +0200

    Update App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 
-import {Home, About, Projects, Contact} from './pages';
+import {Home, About, Projects} from './pages';
 import Navbar from './components/Navbar';
 
 const App = () => {
